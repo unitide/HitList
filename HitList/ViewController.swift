48811ecc02232eacf63b933527211b4eea46d69c
thomas <unitide@gmail.com>
2022-07-24 22:00:59 -0500

    Initial Commit

---
+++ HitList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HitList
+//
+//  Created by Mingyong zhu on 7/24/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
